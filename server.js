0c9929a88dee9f41706428181d5be349d9f24932
Djeck <bertonmarc@gmail.com>
2016-05-21 00:28:32 +0200

    TODO API

--- server.js
+++ server.js
@@ -2,11 +2,34 @@ var express = require("express");
 var app = express();
 var PORT = process.env.PORT || 3000;
 
+var todos = [{
+  id: 1,
+  description: "piscine",
+  completed: false
+},{
+  id: 2,
+  description: "france loisir",
+  completed: false
+},{
+  id: 3,
+  description: "foutu jardin",
+  completed: true
+}];
+
 app.get("/", function(req, res){
   res.send("Todo API Root");
 });
 
+app.get("/todos", function(req,res){
+  res.json(todos);
+});
 
+app.get("/todo/:id", function(req,res){
+  var id = req.params.id;
+  var todo = todos.find(function(item){return item.id == id; });
+  if(todo) res.json(todo);
+  else res.status(404).send();
+});
 
 
 
